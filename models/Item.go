e2891cddd65b742d9d34c16266ca842cd3feb6d6
aiis0125 <aidar.issakhanov@netcracker.com>
2025-04-08 16:38:37 +0500

    changed Item.go

--- models/Item.go
+++ models/Item.go
@@ -3,19 +3,19 @@ package models
 import "time"
 
 type Item struct {
-	ID        int
-	Name      string
-	Brand     string
-	Category  string
-	Size      string
-	Color     string
-	Price     float64
-	BoughtFor float64
-	Sex       string
-	Photo     string
-	Barcode   string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        string    `json:"id" db:"id"`
+	Name      string    `json:"name" db:"name"`
+	Brand     Brand     `json:"brand" db:"brand"`
+	Category  Category  `json:"category" db:"category"`
+	Size      Size      `json:"size" db:"size"`
+	Color     Color     `json:"color" db:"color"`
+	Price     int       `json:"price" db:"price"`
+	BoughtFor int       `json:"bought_for" db:"bought_for"`
+	Sex       string    `json:"sex" db:"sex"`
+	Photo     Photo     `json:"photo" db:"photo"`
+	Barcode   Barcode   `json:"barcode" db:"barcode"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
 
 type Barcode struct {
